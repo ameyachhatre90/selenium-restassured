0410ae12c8beff579cdb854bc07c8b81940b64f8
Ameya Chhatre <ameyachhatre90@gmail.com>
2024-09-02 14:12:47 -0700

    selenium and rest assured

--- src/main/java/utils/Constants.java
+++ src/main/java/utils/Constants.java
@@ -5,5 +5,6 @@ public class Constants {
     public final static String UPGRADE_URL = "upgradeUrl";
     public final static String PUBLIC_EVENTS_PATH = "publicEventsPath";
     public final static String PERSONAL_INFO_PATH = "personalInformationPath";
+    public final static String REFINANCE_CREDIT_CARD_PAGE_DATA_PATH = "refinanceCreditCardPageDataPath";
     public final static String LOGIN_PATH = "loginPath";
 }
---
+++ src/test/java/tests/api/RefinanceCreditCardGetTests.java
@@ -0,0 +1,34 @@
+package tests.api;
+
+import com.google.inject.Inject;
+import io.restassured.http.ContentType;
+import io.restassured.http.Method;
+import model.builders.RequestBuilder;
+import org.testng.annotations.Test;
+import utils.Constants;
+import utils.RestUtils;
+
+import static org.testng.Assert.assertNotNull;
+
+public class RefinanceCreditCardGetTests extends TestBase {
+
+    @Inject
+    private RestUtils restUtils;
+
+    @Test(testName = "Personal Information Credit Card - Success")
+    void personalInformationCreditCardSuccess() {
+
+        RequestBuilder requestBuilder = RequestBuilder.builder()
+                .headers(headers)
+                .method(Method.GET)
+                .basePath(configuration.getProperty(Constants.UPGRADE_URL))
+                .uriPath(configuration.getProperty(Constants.REFINANCE_CREDIT_CARD_PAGE_DATA_PATH))
+                .build();
+        restUtils.restCall(requestBuilder).isOK();
+        restUtils.getResponse().assertThat().contentType(ContentType.JSON);
+        restUtils.restCall(requestBuilder).isOK().isSchemaMatching("schemas/refinanceCreditCardsPageData.json");
+        assertNotNull(restUtils.getJsonResponse().getString("componentChunkName"),
+                "Post public event componentChunkName value is null in the response");
+    }
+
+}
